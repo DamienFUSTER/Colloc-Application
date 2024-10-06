db06cd2af65aa200e878543d917e7b6fa5e8b155
Damien <damienfuster@MacBook-Pro-de-Damien-2.local>
2024-10-06 09:10:46 +0200

    enlever les fichiers dash

--- pages/pg_1.py
+++
@@ -1,8 +0,0 @@
-import dash
-from dash import html
-
-# --- PAGE DECLARATION ---
-dash.register_page(module=__name__, name="Programme", path="/")
-
-layout = html.Div([html.H1("Programme colloc"),
-                   html.H5("nanan le programme")])
--- pages/pg_3.py
+++
@@ -1,8 +0,0 @@
-import dash
-from dash import html
-
-# --- PAGE DECLARATION ---
-dash.register_page(module=__name__, name="Liens utiles")
-
-layout = html.Div([html.H1("Liens utiles"),
-                   html.H5("nanan les liens utiles")])
