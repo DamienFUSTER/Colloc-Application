44390e6e421d2194d6c7525c42a0a9c4496d1fd2
Damien <damienfuster31@gmail.com>
2024-12-07 10:10:53 +0100

    Finalisation de l'application

--- app.py
+++ app.py
@@ -6,17 +6,16 @@ app = Flask(__name__) # creation of the Flask object app
 
 #
 @app.route("/") # route decorator to tell the url flash should use (INDEX)
-def pres_colloc(): # what inside the page 
+def pres_colloc(): # what inside the page
     return render_template("pres_colloc.html")# return the page content
 
 @app.route("/pres_inter") # route decorator to tell the url flash should use (INDEX)
-def pres_inter(): # what inside the page 
+def pres_inter(): # what inside the page
     return render_template("pres_inter.html")# return the page content
 
 @app.route("/links") # route decorator to tell the url flash should use (INDEX)
-def links(): # what inside the page 
+def links(): # what inside the page
     return render_template("links.html")# return the page content
-
 
 
 if __name__ == "__main__":
